50c3d830805a478b0208f7f143f863e128fd3761
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-17 00:35:10 +0000

    sm6225-common: init: Switch to touchpanel dt2w
    Change-Id: I44a515c728fef65b448901f0235733e8b239bff8
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES  := true
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # DT2W
-TARGET_TAP_TO_WAKE_NODE ?= "/proc/tp_gesture"
+TARGET_TAP_TO_WAKE_NODE ?= "/sys/touchpanel/double_tap"
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/configs/hidl/xiaomi_common_framework_compatibility_matrix.xml
@@ -165,9 +165,6 @@ TARGET_COPY_OUT_ODM := odm
 TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
-
-# Power
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # qcom/common tree
 include $(QCOM_COMMON_PATH)/BoardConfigQcom.mk
